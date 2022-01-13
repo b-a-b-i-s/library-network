51c3440fb737579df7359f57b6743a8d576dec26
Babis <63171080+b-a-b-i-s@users.noreply.github.com>
2022-01-13 21:12:26 +0200

    added backend for home and libraries

--- public/js/modal.js
+++ public/js/modal.js
@@ -102,5 +102,17 @@ window.addEventListener("load", () => {
         document.querySelector(`#lib-phone-${newId}`).select();
     
     })
+
+
+    const togglePasswordSignUp = document.querySelector('#togglePassword');
+    const passwordSignUp = document.querySelector('#password-signup');
+
+    togglePasswordSignUp.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = passwordSignUp.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordSignUp.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+    });
 })
 
