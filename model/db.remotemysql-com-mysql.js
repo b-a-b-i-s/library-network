3020cff58e6548ba8dba105c0632a85db5e3fab9
Babis Skeparnakos <63171080+b-a-b-i-s@users.noreply.github.com>
2022-01-16 03:44:41 +0200

    much backend

--- model/db.remotemysql-com-mysql.js
+++ model/db.remotemysql-com-mysql.js
@@ -2,25 +2,38 @@
 require('dotenv').config();
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
+  connectionLimit : 10,
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database : process.env.DATABASE_DATABASE
 });
-
-connection.connect(function(err) {
+ 
+connection.getConnection(function(err) {
   if (err) throw err;
   console.log("Connected to the database!");
-  // connection.query(`SELECT * FROM Βιβλιοθήκη`, function (err, result) {
-  //   if (err) throw err;
-  //   console.log("Result: " + result);
-  //   for (let index = 0; index < result.length; index++) {
-  //       console.log(result[index])
-        
-  //   }
-  // });
 });
+
+// const connection = mysql.createConnection({
+//   host: process.env.DATABASE_HOST,
+//   user: process.env.DATABASE_USERNAME,
+//   password: process.env.DATABASE_PASSWORD,
+//   database : process.env.DATABASE_DATABASE
+// });
+
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected to the database!");
+//   // connection.query(`SELECT * FROM Βιβλιοθήκη`, function (err, result) {
+//   //   if (err) throw err;
+//   //   console.log("Result: " + result);
+//   //   for (let index = 0; index < result.length; index++) {
+//   //       console.log(result[index])
+        
+//   //   }
+//   // });
+// });
 
 
 
