8fd489377c2738925c9142b1452528f20443f719
Babis Skeparnakos <63171080+b-a-b-i-s@users.noreply.github.com>
2022-01-17 01:10:07 +0200

    small pics

--- controller/library-network-controller.js
+++ controller/library-network-controller.js
@@ -410,158 +410,6 @@ exports.newReservation = (req, res) => {
 
 
 
-exports.addMeeting = (req, res) => {
-    model.addMeeting(req.body, req.session.loggedUserId, (err, url) => {
-        if (err) {
-            res.send(err);
-        }
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
-        res.end(url, 'utf-8')
-    });
-}
-
-exports.publish = (req, res) => {    
-    res.render('publish', { 
-        url:req.headers.host+'/meeting/'+req.params.url,
-        shorturl:'/meeting/'+req.params.url,
-        loggedin:true,
-        partialContext: {name: req.session.loggedUserName}
-        // name:req.session.userName
-    })
-}
-    
-
-
-exports.renderVote = (req, res) => {
-    if (!req.session.userId) {
-        model.addTempUser(req, (err, user)=> {
-            if (err) {
-                console.log(err)
-                res.send(err)
-            }
-
-            req.session.userId = user.userId
-            req.session.name = user.name
-            req.session.tempUserId = user.id
-
-
-            async function saveit(){
-                await req.session.save()
-                console.log(req.session)
-                model.checkIfClosedAndIfUserIsCreator(req, (err, closed, check) => {
-                    if (err) {
-                        res.send(err);
-                    }
-                    if (closed){
-                        res.render('vote',{open:false});
-                    }
-                    else {
-                        res.render('vote',{open:true})
-                    }
-                    
-                }); 
-            }
-            saveit();
-
-        })
-    }
-    else {
-        model.checkIfClosedAndIfUserIsCreator(req, (err, closed, check) => {
-            if (err) {
-                res.send(err);
-            }
-            if (closed){
-                if (req.session.loggedUserId) {
-                    res.render('vote',{open:false, isCreator:check, partialContext: {name:req.session.loggedUserName}, loggedin:true});
-                }
-                else {
-                    res.render('vote',{open:false})
-                }
-            }
-            else {
-                if (req.session.loggedUserId) {
-                    res.render('vote',{name:req.session.loggedUserName, open:true, isCreator:check, partialContext: {name:req.session.loggedUserName}, loggedin:true});
-                }
-                else {
-                    res.render('vote',{open:true})
-                }
-            }
-        }); 
-    }
-    
-    
-    
-    
-    
-
-}
-
-
-
-exports.getDates = (req, res) => {
-    let userId = req.session.userId;
-    
-    model.getDates(req, (err, data, votes, meetingInfo, userName) => {
-        if (err) {
-            res.send(err);
-        }
-        if (votes) {
-            votes.forEach(el => {
-                if (el.UserIdVote === userId) el.UserIdVote = true//req.session.useid
-                else el.UserIdVote = false
-            });
-        }
-        res.json( { 
-            data:data,
-            votes:votes,
-            meetingInfo : meetingInfo,
-            thisUserName : userName
-        })
-    });
-}
-
-exports.addVotes = (req, res) => {
-
-    if (req.session.loggedUserName) req.session.loggedUserName = req.params.name
-    
-    model.checkIfClosedAndIfUserIsCreator(req, (err, closed, check) => {
-        if (err) {
-            res.send(err);
-        }
-        if (closed){
-            res.render('vote',{open:false});
-        }
-        else {
-            model.addVotes(req, (err, invalid) => {
-                if (err) {
-                    res.send(err);
-                }
-                if (invalid) {
-                    res.writeHead(200, { 'Content-Type': 'text/plain' })
-                    res.end('invalid', 'utf-8')
-                }
-        
-                res.writeHead(200, { 'Content-Type': 'text/plain' })
-                res.end('votes updated', 'utf-8')
-            });
-        }
-
-    }); 
-
-
-    
-}
-
-exports.chooseFinalOption = (req, res) => {
-    
-    model.chooseFinalOption(req, (err) => {
-        if (err) {
-            res.send(err);
-        }
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
-        res.end('selected final date', 'utf-8')
-    });
-}
 
 
 
@@ -605,9 +453,10 @@ exports.doLogin = function (req, res) {
             
                         async function saveit(){
                             await req.session.save()
-                            console.log(req.session)
+                            // console.log(req.session)
                             const redirectTo = "/loggedin";               
-                            res.render('home', {alert: 'Επιτυχής σύνδεση', style: ["home"]})
+                            res.render('home', {partialContext: {name:req.session.loggedUserName}, loggedin:true}{alert: 'Επιτυχής σύνδεση', style: ["home"]})
+
                         }
                         saveit();
                     }
@@ -621,6 +470,7 @@ exports.doLogin = function (req, res) {
 //Τη χρησιμοποιούμε για να ανακατευθύνουμε στη σελίδα /login όλα τα αιτήματα από μη συνδεδεμένςου χρήστες
 exports.checkAuthenticated = function (req, res, next) {
     //Αν η μεταβλητή συνεδρίας έχει τεθεί, τότε ο χρήστης είναι συνεδεμένος
+
     if(req.originalUrl=="/" && req.session.loggedUserId){
         console.log("efttasa")
         res.render('index', {partialContext: {name:req.session.loggedUserName}, loggedin:true})
@@ -658,7 +508,7 @@ exports.doRegister = function (req, res) {
         }
     }
 
-    console.log(Phones)
+    // console.log(Phones)
     // model.registerUser(req.body.username, req.body.password, (err, result, message) => {
     model.getAllUsers(req, (err, users) => {
 
@@ -666,8 +516,8 @@ exports.doRegister = function (req, res) {
         let checkFalsePhone = 0;
 
         for (let index = 0; index < users.length; index++) {
-            console.log('l', users[index].email, users[index].Αρ_Τηλ)
-            console.log('body', req.body.UserEmail)
+            // console.log('l', users[index].email, users[index].Αρ_Τηλ)
+            // console.log('body', req.body.UserEmail)
             if (users[index].email==req.body.UserEmail && users[index].email!=null && req.body.UserEmail!=null){
                 checkFalseEmail = 1;
                 break;
@@ -699,18 +549,3 @@ exports.doRegister = function (req, res) {
     })
 }
 
-exports.showMyMeetings = function (req, res) {
-    
-    model.showMyMeetings(req, (err, result) => {
-        
-        if (err) {
-            console.error('registration error: ' + err);
-            res.render('index');
-        }
-        else {
-            console.log(result)
-            res.render('mymeetings', {meeting:result, partialContext: {name:req.session.loggedUserName}, loggedin:true})
-
-        }
-    })
-}
