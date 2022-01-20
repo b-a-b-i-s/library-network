d809686e6b9a92feae25373e7ca851589236f0d1
Babis <63171080+b-a-b-i-s@users.noreply.github.com>
2022-01-20 06:11:12 +0200

    coockie

--- app.js
+++ app.js
@@ -55,7 +55,6 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.locals.errorstatus = err.status || 500;
   res.locals.errorstack = req.app.get('env') === 'development' ? err.stack : '';
 
