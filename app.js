0a4f604ef7b4aea89ae74aa7f6e53444c8289630
Babis Skeparnakos <63171080+b-a-b-i-s@users.noreply.github.com>
2022-01-23 02:09:23 +0200

    redis

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const sess = {
 //  
 }
 
-if (app.get('env') === 'productio') {
+if (app.get('env') === 'production') {
   // app.set('trust proxy', 1) // trust first proxy
   sess.cookie.secure = true // serve secure cookies
 
